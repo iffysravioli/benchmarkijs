387585f175a13e39bcf2f59c50fbf6be42c595ae
iffysravioli <21itmj@gmail.com>
2023-02-07 21:34:04 -0500

    Update main.py
    i kept getting a syntax error so i am gonna try again !

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import time
 start_time = time.time()
 result = 0
 for i in range(1010):
-    result = 0 
+    result += i
 for i in range(5*10**9):
-    result *=i 
+    result *= i
 for i in range(2*10**9):
-    result /= i 
-
+    result /= i
