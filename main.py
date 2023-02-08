e1b648ee3664e0a49a72f680692b2e1f6283a088
iffysravioli <21itmj@gmail.com>
2023-02-08 00:01:17 -0500

    currently working on the 64 bit

--- main.py
+++ main.py
@@ -14,3 +14,17 @@ end_time = time.time()
 #calculating & printing the execution time 
 execution_time = end_time - start_time
 print("Integer operation benchmark execution time:", execution_time)
+
+#64-bit Floating Point Operation Benchmark
+import time 
+start_time = time.time()
+
+result = 0.0 
+for i in range(1010):
+    result += i
+for i in range(5*10**9):
+    result *= i 
+for i in range(2*10**9):
+    result /= i 
+
+    
