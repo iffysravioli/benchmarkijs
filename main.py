5edf3f516c2b95f3608f0b481d822b697824342c
iffysravioli <21itmj@gmail.com>
2023-02-07 19:40:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,10 @@ import time
 #timer
 start_time = time.time()
 result = 0
+for i in range(1010):
+    result = 0 
+for i in range(5*10**9):
+    result *=i 
+for i in range(2*10**9):
+    result /= i 
 
