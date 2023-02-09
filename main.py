301accf680266ca6f45233ac35dcdbc35a85a860
iffysravioli <21itmj@gmail.com>
2023-02-09 18:18:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 #32-bit integer operation 
-import time
+#import time
 #timer
-start_time = time.time()
-result = 0
-for i in range(1010):
-    result += i
-for i in range(5*10**9):
-    result *= i
-for i in range(2*10**9):
-    result /= i 
+#start_time = time.time()
+#result = 0
+#for i in range(1010):
+    #result += i
+#for i in range(5*10**9):
+    #result *= i
+#for i in range(2*10**9):
+    #result /= i 
 #stop timer
-end_time = time.time()
+#end_time = time.time()
 #calculating & printing the execution time 
-execution_time = end_time - start_time
-print("Integer operation benchmark execution time:", execution_time)
+#execution_time = end_time - start_time
+#print("Integer operation benchmark execution time:", execution_time)
 
 #64-bit Floating Point Operation Benchmark
 import time 
 start_time = time.time()
 
-result = 0.0 
+result = 1.0 
 for i in range(1010):
     result += i
 for i in range(5*10**9):
