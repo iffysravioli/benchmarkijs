6680e1cf040ea5035d0a599f5b8d6290c81ce6cb
iffysravioli <21itmj@gmail.com>
2023-02-09 21:28:16 -0500

    Update main.py
    my computer keeps freezing

--- main.py
+++ main.py
@@ -1,35 +1,30 @@
+import time 
+
 #32-bit integer operation 
-#import time
-#timer
-#start_time = time.time()
-#result = 0
-#for i in range(1010):
-    #result += i
-#for i in range(5*10**9):
-    #result *= i
-#for i in range(2*10**9):
-    #result /= i 
-#stop timer
-#end_time = time.time()
-#calculating & printing the execution time 
-#execution_time = end_time - start_time
-#print("Integer operation benchmark execution time:", execution_time)
+def int_operation():
+    start_time = time.time()
+    value = 1 
+    for num in range(10**10):
+        value += num 
+    for num in range(5*10**9):
+        value *= num
+    for num in range(2*10**9):
+        value /= num 
+    end_time = time.time()
+    execution_time = end_time - start_time 
+    print("The 32-bit Integer operation benchmark runtime: ", execution_time)
+int_operation()
 
 #64-bit Floating Point Operation Benchmark
-import time 
-start_time = time.time()
-
-result = 1.0 
-for i in range(1010):
-    result += i
-for i in range(5*10**9):
-    result *= i 
-for i in range(2*10**9):
-    result /= i 
-
-#to stop the timer 
-end_time = time.time()
-
-#calculate & print its exection time 
-execution_time = end_time - start_time
-print("64-floating point operation benchmark execution time:", execution_time)
+def float_point():
+    start_time = time.time()
+    value = 1.0 
+    for i in range(10**10):
+        value +- i 
+    for i in range((5*10)**9):
+        value *= i 
+    for i in range((2*10)**9):
+        value /= i 
+    stop_timer2 = time.time()
+    execution_time2 = stop_timer2 - start_time
+    print("The 64-bit Floating Point Opreation benchmark runtime is ", execution_time2)
