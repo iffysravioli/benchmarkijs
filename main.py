30df14074cc499079e9d7071fbd3a5dff70f3138
iffysravioli <111910162+iffysravioli@users.noreply.github.com>
2023-02-10 18:48:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time 
 
 #32-bit integer operation 
-def integer_op():
+def integer_operation():
     start_time = time.time()
     count = 1 
     for i in range(10**10):
@@ -13,49 +13,58 @@ def integer_op():
     end_time = time.time()
     execution_time = end_time - start_time 
     print("The 32-bit Integer operation benchmark execution time: ", execution_time)
-integer_op()
+integer_operation()
 
 #64-bit Floating Point Operation Benchmark
-#def float_point():
-    #start_time = time.time()
-    #value = 1.0 
-    #for i in range(10**10):
-        #value +- i 
-    #for i in range((5*10)**9):
-        #value *= i 
-    #for i in range((2*10)**9):
-        #value /= i 
-    #stop_timer2 = time.time()
-    #execution_time2 = stop_timer2 - start_time
-    #print("The 64-bit Floating Point Opreation benchmark execution time is ", execution_time2)
-#float_point()
+def float_point():
+    start_time2 = time.time()
+    value = 1.0 
+    for i in range(10**10):
+        value +- i 
+    for i in range((5*10)**9):
+        value *= i 
+    for i in range((2*10)**9):
+        value /= i 
+    end_timer2 = time.time()
+    execution_time2 = end_timer2 - start_time2
+    print("The 64-bit Floating Point Opreation benchmark execution time is ", execution_time2)
+float_point()
 
 #Memory 
-#def memory():
-    #start_time = time()
-    #for i in range(10**5):
-        #array[i] = i 
-        #value = array[i]
-        #stop_timer3 = time.time()
-        #runtime3 = stop_timer3 - timer3 
-        #print("The Memory Benchmark execution time is ", execution_time3)
-        
+arrary = ((5*10)**4)
+def memory():
+    start_time3 = time.time()
+    for i in range(10**5):
+        arrary[i] = i 
+        value = arrary[i]
+        end_timer3 = time.time()
+        execution_time3 = end_timer3 - start_time3 
+        print("The Memory Benchmark execution time is ", execution_time3)
+memory()
     
 #Hard Drive 1 
-#def hard_drive():
-    #timer4 - time.time()
-    #with open("BenchmarkProject.txt", "wb") as f: 
-        #for i in range(10**9):
-            #f.write(100)
-        #with open("BenchmarkProject.txt","rb") as f: 
-            #for i in range(10**9):
-                #f.read(100)
-            #stop_timer4 = time.time()
-            #runtime4 = stop_timer4 - timer4 
-            #print("The Hard drive #1 benchmark exe")
+def harddrive1():
+    start_timer4 = time.time()
+    with open("BenchmarkProject.txt", "wb") as f: 
+        for i in range(10**9):
+            f.write(100)
+        with open("BenchmarkProject.txt","rb") as f: 
+            for i in range(10**9):
+                f.read(100)
+            end_timer4 = time.time()
+            execution_time4 = end_timer4 - start_timer4 
+            print("The Hard drive 1 benchmark runtime is ", execution_time4)
 
 
 #Hard Drive 2 
-#def hand_drive2():
-#   timer
-    
+def harddrive2():
+    start_timer5 = time.time()
+    with open("BenchmarkProject.txt", "wb") as f: 
+        for i in range(10**9):
+            f.write(100)
+        with open("BenchmarkProject.txt","rb") as f: 
+            for i in range(10**9):
+                f.read(100)
+            end_timer5 = time.time()
+            execution_time5 = end_timer5 - start_timer5 
+            print("The Hard drive 1 benchmark runtime is ", execution_time5)
